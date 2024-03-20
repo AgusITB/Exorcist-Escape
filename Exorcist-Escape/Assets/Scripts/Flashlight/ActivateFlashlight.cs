149e81e88bef88090b7f4dd198248a43ae13594e
Samuel-Arteaga <samuel.arteaga.7e6@itb.cat>
2024-03-20 13:18:57 +0100

    update activate y desactivate luz

--- Exorcist-Escape/Assets/Scripts/Flashlight/ActivateFlashlight.cs
+++ Exorcist-Escape/Assets/Scripts/Flashlight/ActivateFlashlight.cs
@@ -4,40 +4,65 @@ using UnityEngine;
 
 public class ActivateFlashlight : MonoBehaviour
 {
-    public Light normalLight; 
-    public Light uvLight; 
-    //public GameObject objecto;
+    public Light normalLight;
+    public Light uvLight;
     public bool insidePurple;
-
-    //private void OnTriggerStay(Collider other)
-    //{
-    //    if(other.CompareTag("Purple"))
-    //    {
-    //        insidePurple = true;
-    //    }
-    //}
-    //private void OnTriggerExit(Collider other)
-    //{
-    //    if (other.CompareTag("Purple"))
-    //    {
-    //        insidePurple = false;
-    //    }
-    //}
-
 
     void Update()
     {
-        //gameObject.SetActive(insidePurple);
-
+        // Verificar si la linterna está activada
         if (InputManager.Instance.IsFlashing())
+        {
+            // Desactivar ambas luces si la linterna está activada
+            normalLight.enabled = true;
+            uvLight.enabled = false;
+        }
+        else if (InputManager.Instance.IsFlashing())
         {
             normalLight.enabled = false;
             uvLight.enabled = true;
         }
         else
         {
-            normalLight.enabled = true;
-            uvLight.enabled = false;
+            // Si la linterna no está activada, verificar si se presionó la tecla Z
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                // Si la luz normal está activada, activar la luz UV y desactivar la luz normal
+                if (normalLight.enabled)
+                {
+                    normalLight.enabled = false;
+                    uvLight.enabled = true;
+                }
+                // Si la luz UV está activada, activar la luz normal y desactivar la luz UV
+                else if (uvLight.enabled)
+                {
+                    normalLight.enabled = true;
+                    uvLight.enabled = false;
+                }
+            }
+          
+
         }
+        //gameObject.SetActive(insidePurple);
+
+        //if (InputManager.Instance.IsFlashing())
+        //{
+        //    normalLight.enabled = false;
+        //    uvLight.enabled = false;
+        //}
+        //else
+        //{
+        //    //normalLight.enabled = true;
+        //    if (Input.GetKeyDown(KeyCode.Z))
+        //    {
+        //        uvLight.enabled = true;
+        //        //normalLight.enabled = false;
+        //    }
+        //    else
+        //    {
+        //        uvLight.enabled = false;
+        //        normalLight.enabled = true;
+        //    }
+        //}
     }
 }
--- Exorcist-Escape/Assets/Scripts/Player/InputManager.cs
+++ Exorcist-Escape/Assets/Scripts/Player/InputManager.cs
@@ -6,7 +6,9 @@ public class InputManager : MonoBehaviour
 
     private static InputManager instance;
 
-    private bool isFlashingActive = false; 
+    private bool isFlashingActive = false;
+    private bool isUVActive = false;
+
 
     public static InputManager Instance
     {
@@ -23,7 +25,8 @@ public class InputManager : MonoBehaviour
 
 
         playerControls = new PlayerControls();
-        playerControls.Player.Flashlight.started += ctx => ToggleFlashlight(); 
+        playerControls.Player.Flashlight.started += ctx => ToggleFlashlight();
+        playerControls.Player.ToggleLights.started += ctx => ToggleLights();
     }
 
     private void OnEnable()
@@ -54,5 +57,11 @@ public class InputManager : MonoBehaviour
     public void ToggleFlashlight()
     {
         isFlashingActive = !isFlashingActive;
+
     }
+    public void ToggleLights()
+    {
+        isUVActive = !isUVActive;
+    }
+
 }
