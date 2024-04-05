09bf7f005b454b2b24b819569ad9cbd9449cac35
AgusITB <agustin.garibotto.7e6@itb.cat>
2024-04-05 15:50:57 +0200

    si

--- Exorcist-Escape/Assets/Scripts/Player/AudioLoudnessDetector.cs
+++ Exorcist-Escape/Assets/Scripts/Player/AudioLoudnessDetector.cs
@@ -6,6 +6,22 @@ public class AudioLoudnessDetector : MonoBehaviour
 {
     public int sampleWindow = 64;
 
+    private AudioClip microphoneClip;
+
+    private void Start()
+    {
+        MicrophoneToAudioClip(0);
+    }
+    private void MicrophoneToAudioClip(int microphoneIndex)
+    {
+        foreach (var name in Microphone.devices)
+        {
+            Debug.Log(name);
+        }
+
+        //string microphoneName = Microphone.devices[1];
+        Debug.Log("No hay na");
+    }
     public float GetLoudnessFromAudioClip(int clipPosition, AudioClip clip)
     {
         int startPosition = clipPosition - sampleWindow;
