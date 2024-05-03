f9a0b47a122fc97c609f564bcfc0de4813d6bdc0
AgusITB <agustin.garibotto.7e6@itb.cat>
2024-05-03 15:54:45 +0200

    Big gates fixed

--- Exorcist-Escape/Assets/Scripts/PlayerAim.cs
+++ Exorcist-Escape/Assets/Scripts/PlayerAim.cs
@@ -50,18 +50,18 @@ public class PlayerAim : MonoBehaviour
         Debug.DrawLine(headPos.position, headPos.TransformDirection(Vector3.forward) * Mathf.Infinity, Color.red);
         int layerMask = 1 << 8;
 
-        if (Physics.Raycast(headPos.position, headPos.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+        if (Physics.Raycast(headPos.position, headPos.TransformDirection(Vector3.forward), out hit, 1.5f, layerMask))
         {
             Debug.DrawRay(headPos.position, headPos.TransformDirection(Vector3.forward) * hit.distance, Color.red);
 
-            float distance = Vector3.Distance(transform.position, hit.transform.position);
-            if (distance <= 1.5f)
-            {
+            //float distance = Vector3.Distance(transform.position, hit.collider.transform.position);
+            //if (distance <= 1.5f)
+            //{
                     if (hit.transform.TryGetComponent(out IInteractable gameobject))
                     {
                         gameobject.Interact();
                     };
-            }
+            //}
         }
     }
 }
